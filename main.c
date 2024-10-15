521f34ea235fe9133cfffb4b553ee676f5069e74
jefer30039 <jefer30039@gmail.com>
2024-10-15 01:20:18 -0600

    Agregar botones numéricos a la calculadora

--- main.c
+++ main.c
@@ -1,15 +1,5 @@
 #include <gtk/gtk.h>
 
-typedef struct {
-    int a;
-    int b;
-} SumData;
-
-static void sum(GtkWidget *widget, gpointer data) {
-    SumData *sum_data = (SumData *) data;
-    g_print("Sum: %d\n", sum_data->a + sum_data->b);
-}
-
 static void say_hello(GtkWidget *widget, gpointer data) {
     g_print("Hello, World!\n");
 }
@@ -22,28 +12,62 @@ static void activate(GtkApplication *application, gpointer user_data) {
 
     window = gtk_application_window_new(application);
     gtk_window_set_title(GTK_WINDOW(window), "Calculadora");
-    gtk_window_set_default_size(GTK_WINDOW(window), 200, 200);
+    gtk_window_set_default_size(GTK_WINDOW(window), 170, 180);
 
     grid = gtk_grid_new();
     gtk_window_set_child(GTK_WINDOW(window), grid);
 
-    label = gtk_label_new("Resultado");
+    label = gtk_label_new("Result");
     gtk_grid_attach(GTK_GRID(grid), label, 0, 0, 1, 1);
 
-    button = gtk_button_new_with_label("AC");
+    button = gtk_button_new_with_label("AC"); // Clear
     g_signal_connect(button, "clicked", G_CALLBACK(say_hello), NULL);
     gtk_grid_attach(GTK_GRID(grid), button, 0, 1, 1, 1);
 
-    //for para crear todos los botones numericos
-    for (int i = 0; i < 10; i++) {
+    button = gtk_button_new_with_label("<"); // Backspace
+    g_signal_connect(button, "clicked", G_CALLBACK(say_hello), NULL);
+    gtk_grid_attach(GTK_GRID(grid), button, 1, 1, 1, 1);
+
+    button = gtk_button_new_with_label("%"); // Porcentage
+    g_signal_connect(button, "clicked", G_CALLBACK(say_hello), NULL);
+    gtk_grid_attach(GTK_GRID(grid), button, 2, 1, 1, 1);
+
+    button = gtk_button_new_with_label("÷"); // Division
+    g_signal_connect(button, "clicked", G_CALLBACK(say_hello), NULL);
+    gtk_grid_attach(GTK_GRID(grid), button, 3, 1, 1, 1);
+
+    button = gtk_button_new_with_label("x"); // Multiplicacion
+    g_signal_connect(button, "clicked", G_CALLBACK(say_hello), NULL);
+    gtk_grid_attach(GTK_GRID(grid), button, 3, 2, 1, 1);
+
+    button = gtk_button_new_with_label("-"); // Resta
+    g_signal_connect(button, "clicked", G_CALLBACK(say_hello), NULL);
+    gtk_grid_attach(GTK_GRID(grid), button, 3, 3, 1, 1);
+
+    button = gtk_button_new_with_label("+"); // Suma
+    g_signal_connect(button, "clicked", G_CALLBACK(say_hello), NULL);
+    gtk_grid_attach(GTK_GRID(grid), button, 3, 4, 1, 1);
+
+    button = gtk_button_new_with_label("="); // Igual
+    g_signal_connect(button, "clicked", G_CALLBACK(say_hello), NULL);
+    gtk_grid_attach(GTK_GRID(grid), button, 3, 5, 1, 1);
+
+    button = gtk_button_new_with_label("."); // Punto
+    g_signal_connect(button, "clicked", G_CALLBACK(say_hello), NULL);
+    gtk_grid_attach(GTK_GRID(grid), button, 2, 5, 1, 1);
+
+    //for para crear los botones de los numeros (menos el 0)
+    for (int i = 1; i < 10; i++) {
         button = gtk_button_new_with_label(g_strdup_printf("%d", i));
         g_signal_connect(button, "clicked", G_CALLBACK(say_hello), NULL);
-        gtk_grid_attach(GTK_GRID(grid), button, i % 3, i / 3 + 2, 1, 1);
+        gtk_grid_attach(GTK_GRID(grid), button, (i - 1) % 3, 4 - (i - 1) / 3, 1, 1);
     }
 
+    button = gtk_button_new_with_label("0");
+    g_signal_connect(button, "clicked", G_CALLBACK(say_hello), NULL);
+    gtk_grid_attach(GTK_GRID(grid), button, 0, 5, 2, 1);
 
     gtk_window_present(GTK_WINDOW(window));
-
 
 }
 
