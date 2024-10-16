6325aa85cf35179f06c6188e350ae7e11e6677b9
jefer30039 <jefer30039@gmail.com>
2024-10-16 10:41:39 -0600

    Se refactorizaron los nombres de las funciones relacionadas con las operaciones matemáticas en la calculadora. Se corrige el error en la llamada de los numeros

--- main.c
+++ main.c
@@ -1,53 +1,120 @@
 #include <gtk/gtk.h>
+#include <string.h>
 
-extern void suma(GtkWidget *widget, gpointer data);
-extern void resta(GtkWidget *widget, gpointer data);
-extern void multiplicacion(GtkWidget *widget, gpointer data);
-extern void division(GtkWidget *widget, gpointer data);
-extern void porcentaje(GtkWidget *widget, gpointer data);
+extern void calcularSuma(int operando1, int operando2);
+extern void calcularResta(int operando1, int operando2);
+extern void calcularMultiplicacion(int operando1, int operando2);
+extern void calcularDivision(int operando1, int operando2);
+extern void calcularPorcentaje(int operando1, int operando2);
+
+int numero = 0;
+extern int operando1 = 0;
+extern int operando2 = 0;
+int operacion = 0;
+int resultado = 0;
 
 static void say_hello (GtkWidget *widget, gpointer data) {
     g_print("Hello World\n");
 }
-
-static void cero (GtkWidget *widget, gpointer data) {
-    g_print("0\n");
+static void suma (GtkWidget *widget, gpointer data) {
+    g_print("+\n");
+    operando1 = numero;
+    numero = 0;
+    operacion = 1;
+}
+static void resta (GtkWidget *widget, gpointer data) {
+    g_print("-\n");
+    operando1 = numero;
+    numero = 0;
+    operacion = 2;
+}static void multiplicacion (GtkWidget *widget, gpointer data) {
+    g_print("x\n");
+    operando1 = numero;
+    numero = 0;
+    operacion = 3;
+}static void division (GtkWidget *widget, gpointer data) {
+    g_print("÷\n");
+    operando1 = numero;
+    numero = 0;
+    operacion = 4;
+}static void porcentaje (GtkWidget *widget, gpointer data) {
+    g_print("porcetaje\n");
+    operando1 = numero;
+    numero = 0;
+    operacion = 5;
+}static void ac (GtkWidget *widget, gpointer data) {
+    g_print("AC\n");
+}
+static void igual(GtkWidget *widget, gpointer data) {
+    g_print("=\n");
+    operando2 = numero;
+    switch (operacion){
+        case 1:
+            calcularSuma(widget, NULL);
+            break;
+        case 2:
+            calcularResta(widget, NULL);
+            break;
+        case 3:
+            calcularMultiplicacion(widget, NULL);
+            break;
+        case 4:
+            calcularDivision(widget, NULL);
+            break;
+        case 5:
+            calcularPorcentaje(widget, NULL);
+            break;
+        default:
+            break;
+    }
 }
 
-static void uno (GtkWidget *widget, gpointer data) {
-    g_print("1\n");
-}
-
-static void dos (GtkWidget *widget, gpointer data) {
-    g_print("2\n");
-}
-
-static void tres (GtkWidget *widget, gpointer data) {
-    g_print("3\n");
-}
-
-static void cuatro (GtkWidget *widget, gpointer data) {
-    g_print("4\n");
-}
-
-static void cinco (GtkWidget *widget, gpointer data) {
-    g_print("5\n");
-}
-
-static void seis (GtkWidget *widget, gpointer data) {
-    g_print("6\n");
-}
-
-static void siete (GtkWidget *widget, gpointer data) {
-    g_print("7\n");
-}
-
-static void ocho (GtkWidget *widget, gpointer data) {
-    g_print("8\n");
-}
-
-static void nueve (GtkWidget *widget, gpointer data) {
-    g_print("9\n");
+static void saveNumber(GtkWidget *widget, gpointer data) {
+    switch (GPOINTER_TO_INT(data))
+    {
+    case 0:
+        numero += GPOINTER_TO_INT(data);
+        numero = numero * 10;
+        break;
+    case 1:
+        numero += GPOINTER_TO_INT(data);
+        numero = numero * 10;
+        break;
+    case 2:
+        numero += GPOINTER_TO_INT(data);
+        numero = numero * 10;
+        break;
+    case 3:
+        numero += GPOINTER_TO_INT(data);
+        numero = numero * 10;
+        break;
+    case 4:
+        numero += GPOINTER_TO_INT(data);
+        numero = numero * 10;
+        break;
+    case 5:
+        numero += GPOINTER_TO_INT(data);
+        numero = numero * 10;
+        break;
+    case 6:
+        numero += GPOINTER_TO_INT(data);
+        numero = numero * 10;
+        break;
+    case 7:
+        numero += GPOINTER_TO_INT(data);
+        numero = numero * 10;
+        break;
+    case 8:
+        numero += GPOINTER_TO_INT(data);
+        numero = numero * 10;
+        break;
+    case 9:
+        numero += GPOINTER_TO_INT(data);
+        numero = numero * 10;
+        break;
+    default:
+        break;
+    }
 }
 
 static void punto (GtkWidget *widget, gpointer data) {
@@ -71,7 +138,7 @@ static void activate(GtkApplication *application, gpointer user_data) {
     gtk_grid_attach(GTK_GRID(grid), label, 0, 0, 1, 1);
 
     button = gtk_button_new_with_label("AC"); // Clear
-    g_signal_connect(button, "clicked", G_CALLBACK(say_hello), NULL);
+    g_signal_connect(button, "clicked", G_CALLBACK(ac), NULL);
     gtk_grid_attach(GTK_GRID(grid), button, 0, 1, 1, 1);
 
     button = gtk_button_new_with_label("<"); // Backspace
@@ -79,27 +146,27 @@ static void activate(GtkApplication *application, gpointer user_data) {
     gtk_grid_attach(GTK_GRID(grid), button, 1, 1, 1, 1);
 
     button = gtk_button_new_with_label("%"); // Porcentage
-    g_signal_connect(button, "clicked", G_CALLBACK(say_hello), NULL);
+    g_signal_connect(button, "clicked", G_CALLBACK(porcentaje), NULL);
     gtk_grid_attach(GTK_GRID(grid), button, 2, 1, 1, 1);
 
     button = gtk_button_new_with_label("÷"); // Division
-    g_signal_connect(button, "clicked", G_CALLBACK(say_hello), NULL);
+    g_signal_connect(button, "clicked", G_CALLBACK(division), NULL);
     gtk_grid_attach(GTK_GRID(grid), button, 3, 1, 1, 1);
 
     button = gtk_button_new_with_label("x"); // Multiplicacion
-    g_signal_connect(button, "clicked", G_CALLBACK(say_hello), NULL);
+    g_signal_connect(button, "clicked", G_CALLBACK(multiplicacion), NULL);
     gtk_grid_attach(GTK_GRID(grid), button, 3, 2, 1, 1);
 
     button = gtk_button_new_with_label("-"); // Resta
-    g_signal_connect(button, "clicked", G_CALLBACK(say_hello), NULL);
+    g_signal_connect(button, "clicked", G_CALLBACK(resta), NULL);
     gtk_grid_attach(GTK_GRID(grid), button, 3, 3, 1, 1);
 
     button = gtk_button_new_with_label("+"); // Suma
-    g_signal_connect(button, "clicked", G_CALLBACK(say_hello), NULL);
+    g_signal_connect(button, "clicked", G_CALLBACK(suma), NULL);
     gtk_grid_attach(GTK_GRID(grid), button, 3, 4, 1, 1);
 
     button = gtk_button_new_with_label("="); // Igual
-    g_signal_connect(button, "clicked", G_CALLBACK(say_hello), NULL);
+    g_signal_connect(button, "clicked", G_CALLBACK(igual), NULL);
     gtk_grid_attach(GTK_GRID(grid), button, 3, 5, 1, 1);
 
     button = gtk_button_new_with_label("."); // Punto
@@ -109,12 +176,12 @@ static void activate(GtkApplication *application, gpointer user_data) {
     //for para crear los botones de los numeros (menos el 0)
     for (int i = 1; i < 10; i++) {
         button = gtk_button_new_with_label(g_strdup_printf("%d", i));
-        g_signal_connect(button, "clicked", G_CALLBACK(say_hello), NULL);
+        g_signal_connect(button, "clicked", G_CALLBACK(saveNumber), GINT_TO_POINTER(i));
         gtk_grid_attach(GTK_GRID(grid), button, (i - 1) % 3, 4 - (i - 1) / 3, 1, 1);
     }
 
     button = gtk_button_new_with_label("0");
-    g_signal_connect(button, "clicked", G_CALLBACK(cero), NULL);
+    g_signal_connect(button, "clicked", G_CALLBACK(saveNumber), 0);
     gtk_grid_attach(GTK_GRID(grid), button, 0, 5, 2, 1);
 
     gtk_window_present(GTK_WINDOW(window));
